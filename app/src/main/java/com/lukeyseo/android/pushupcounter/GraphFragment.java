ed08d7cfff1391dd0368b30760fb4d3adbf99f7a
Luke Seo <lukeyseo@gmail.com>
2017-07-10 23:47:27 -0700

    Updates for List/Graph fragments

---
+++ app/src/main/java/com/lukeyseo/android/pushupcounter/GraphFragment.java
@@ -0,0 +1,8 @@
+package com.lukeyseo.android.pushupcounter;
+
+/**
+ * Created by Luke on 7/10/17.
+ */
+
+public class GraphFragment {
+}
--- app/src/main/java/com/lukeyseo/android/pushupcounter/HistoryActivity.java
+++ app/src/main/java/com/lukeyseo/android/pushupcounter/HistoryActivity.java
@@ -1,9 +1,11 @@
 package com.lukeyseo.android.pushupcounter;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.content.res.Configuration;
+import android.support.v4.app.*;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
 
@@ -14,16 +16,22 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
+
         FragmentManager fm = getSupportFragmentManager();
 
         // Check if fragment already exists
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
-            fragment = new CountFragment();
+            fragment = new com.lukeyseo.android.pushupcounter.ListFragment();
 
             // Creates and commits a fragment transaction
             fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+
         }
     }
 }
