170130d5ff6c6e702a6b00cb0d2b33785ceb0505
Luke Seo <lukeyseo@gmail.com>
2017-07-12 22:23:23 -0700

    Display date for x Axis on graph

--- app/src/main/java/com/lukeyseo/android/pushupcounter/GraphFragment.java
+++ app/src/main/java/com/lukeyseo/android/pushupcounter/GraphFragment.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -37,21 +39,61 @@ public class GraphFragment extends Fragment {
 
         mLineChart = (LineChart) getActivity().findViewById(R.id.graph_display);
 
+        // Get list of all push ups
         PushupList pushupList = PushupList.get(getActivity(), "");
-
         List<Pushup> pushups = pushupList.getPushups();
-        List<Entry> entries = new ArrayList<Entry>();
-        int i = 0;
+        String[] formatedDates = new String[pushups.size()];
 
+        // Initialize list for data entries
+        List<Entry> entries = new ArrayList<>();
+
+        int i = 0;
         for (Pushup pushup : pushups) {
             entries.add(new Entry(i, pushup.getCount()));
+
+            // Parse date so it's in the format '07-12' for month/day
+            String dateParsed = pushup.getDate();
+            dateParsed = dateParsed.substring(5);
+            formatedDates[i] = dateParsed;
+
             i++;
         }
 
-        LineDataSet dataSet = new LineDataSet(entries, "Label");
+        if (pushups.size() > 1) {
+            // Set up data for plot
+            LineDataSet dataSet = new LineDataSet(entries, "Label");
+            LineData lineData = new LineData(dataSet);
 
-        LineData lineData = new LineData(dataSet);
-        mLineChart.setData(lineData);
+            // Settings for x axis
+            XAxis xAxis = mLineChart.getXAxis();
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+            xAxis.setDrawGridLines(false);
+            xAxis.setValueFormatter(new MyXAxisValueFormatter(formatedDates));
+            xAxis.setGranularity(1f);
+
+            // Settings for right y axis
+            YAxis yAxis = mLineChart.getAxisRight();
+            yAxis.setDrawLabels(false);
+            yAxis.setEnabled(false);
+
+            mLineChart.setData(lineData);
+        }
+
         mLineChart.invalidate();
+    }
+
+    public class MyXAxisValueFormatter implements IAxisValueFormatter {
+
+        private String[] mValues;
+
+        public MyXAxisValueFormatter(String[] values) {
+            this.mValues = values;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            // "value" represents the position of the label on the axis (x or y)
+            return mValues[(int) value];
+        }
     }
 }
